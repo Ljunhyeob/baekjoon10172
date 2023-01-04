88065cfb096b70fd16813653a51a3f054bfbb9b0
이준협 <121070166+Ljunhyeob@users.noreply.github.com>
2023-01-04 13:29:27 +0900

    Add files via upload

---
+++ baekjoon10172/baekjoon10172/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  baekjoon10172
+//
+//  Created by 이준협 on 2023/01/04.
+//
+
+import Foundation
+
+print(#"|\_/|"#)
+print(#"|q p|   /}"#)
+print(#"( 0 )"""\"#)
+print(#"|"^"`    |"#)
+print(#"||_/=\\__|"#)
